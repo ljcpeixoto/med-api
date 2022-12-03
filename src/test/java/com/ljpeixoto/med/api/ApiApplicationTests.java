ae06a3a1fa87da7c87a226ba67cf30bf2b5dcdb9
Leonardo Peixoto <ljpeixoto@gmail.com>
2022-12-03 11:27:27 -0300

    First commit, right after generating project in Spring Initializer

---
+++ src/test/java/com/ljpeixoto/med/api/ApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ljpeixoto.med.api;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
