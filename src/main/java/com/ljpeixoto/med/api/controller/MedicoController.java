69ba902f9d6b34ca7f5c62320f36be7f7614bc47
Leonardo Peixoto <ljpeixoto@gmail.com>
2022-12-05 07:22:08 -0300

    Add valitation to DadosCadastroMedico

--- src/main/java/com/ljpeixoto/med/api/controller/MedicoController.java
+++ src/main/java/com/ljpeixoto/med/api/controller/MedicoController.java
@@ -3,6 +3,7 @@ package com.ljpeixoto.med.api.controller;
 import com.ljpeixoto.med.api.medico.DadosCadastroMedico;
 import com.ljpeixoto.med.api.medico.Medico;
 import com.ljpeixoto.med.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ public class MedicoController {
 
     @PostMapping()
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
     }
 }
