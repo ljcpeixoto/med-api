56d9f1875dd3a2df3d09480b43c75075790f26c8
Leonardo Peixoto <ljpeixoto@gmail.com>
2022-12-06 08:00:49 -0300

    Alter service of listing Medicos to paginate results

--- src/main/java/com/ljpeixoto/med/api/controller/MedicoController.java
+++ src/main/java/com/ljpeixoto/med/api/controller/MedicoController.java
@@ -6,10 +6,10 @@ import com.ljpeixoto.med.api.medico.Medico;
 import com.ljpeixoto.med.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -25,7 +25,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
