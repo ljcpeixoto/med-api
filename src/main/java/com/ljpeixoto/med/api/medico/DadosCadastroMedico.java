8cf8f36edbe935a0df3ffd50f5ae65173a4e2a37
Leonardo Peixoto <ljpeixoto@gmail.com>
2022-12-06 07:42:17 -0300

    Add field telefone to Medico entity

--- src/main/java/com/ljpeixoto/med/api/medico/DadosCadastroMedico.java
+++ src/main/java/com/ljpeixoto/med/api/medico/DadosCadastroMedico.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.Pattern;
 public record DadosCadastroMedico(
         @NotBlank String nome,
         @NotBlank @Email String email,
+         @NotBlank String telefone,
         @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
         @NotNull Especialidade especialidade,
         @NotNull @Valid DadosEndereco endereco
